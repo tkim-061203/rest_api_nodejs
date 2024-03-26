3b6fddeabddd3d0626e6d1a5f7c9a65df0e7b6dd
tkim-011203 <nntkim.work@gmail.com>
2024-03-27 00:42:13 +0700

    Edit server

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const path = require('path')
-//chinh templating
-app.set('views', path.join(__dirname, 'view'))
-app.set('view engine', 'ejs')
-
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL)
     .then(()=>{
        console.log("DB connection successful.");
@@ -16,13 +11,11 @@ mongoose.connect(process.env.DATABASE_URL)
        console.log(`DB connection error:${err}`);
     });
 
-app.use(express.json())
-const bpmRouter = require("./routes/BPMs")
-app.use('/BPM',bpmRouter)
+app.use(express.json());
+const bpmRouter = require("./routes/BPMs");
+app.use('/BPM',bpmRouter);
 
-app.get( '/', (req, res) => {
-// res.send('<hl>hoi dan it voi Eric </hl>')
-   res.render('sample.ejs')})
 
-app.listen(process.env.PORT || 8081, () => console.log( 'Server Started' ) )
+app.listen(process.env.PORT || 8080, () => {
+   console.log( 'Server Started' )} )
 
