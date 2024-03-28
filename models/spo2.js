0cf3aab80247cfefa0c6857b60a60a614ce9e0be
tkim-011203 <nntkim.work@gmail.com>
2024-03-28 14:32:59 +0700

    edit spo2

---
+++ models/spo2.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const dataSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    data:{
+        type: Number,
+        min : 1,
+        max : 999,
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('bpm',dataSchema)
--- routes/SpO2.js
+++ routes/SpO2.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Subscriber = require('../models/mongodb_model' )
+const Subscriber = require('../models/bpms' )
 // Getting all
 router.get('/', async(req, res) => {
     try {
