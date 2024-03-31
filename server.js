06fd5bdefc6166bf36a94cd11c5bb9f5d13ab134
tkim-011203 <nntkim.work@gmail.com>
2024-03-31 18:29:23 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
 mongoose.connect(process.env.DATABASE_URL)
     .then(()=>{
        console.log("DB connection successful.");
